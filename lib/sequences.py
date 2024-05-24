e3bc86b908249a08ff9f1aed525c9b9cd0995d31
wahome <wahomewgitonga@gmail.com>
2024-05-24 15:30:50 +0300

    update

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if length == 0:
+        print('[]')
+
+    elif length == 1:
+        print('[0]')
+
+    elif length == 2:
+        print('[0, 1]')
+
+    else:
+        fib = [0, 1]
+        while len(fib) < length:
+            fib.append(fib[-1] + fib[-2])
+        print(fib)
